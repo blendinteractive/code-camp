ddda38fa0267124ed13bb1b82b62730cb7541779
sclarson <seth.larson@gmail.com>
2013-07-09 00:12:07 -0500

    Fixes cucumber tests to use actual speaker, for some reason minitest looks like it is running after you bundle exec cucumber though

--- features/step_definitions/codecamp_steps.rb
+++ features/step_definitions/codecamp_steps.rb
@@ -5,14 +5,11 @@ Given(/^there are no upcoming speakers$/) do
 end
 
 Given(/^there are upcoming sessions$/) do
-  Session.create!(:speaker =>"Rod Paddock", :title => "Intro to Rails", :abstract => "If you like asp.net mvc but want to write less code check this out")
-  Session.create!(:speaker =>"Jason Bock", :title => "Metaprogramming .Net", :abstract => "Understanding the concepts, advantages, and pitfalls of metaprogramming")
+  create_speakers_and_sessions
 end
 
 Given(/^there are upcoming speakers$/) do
-  Speaker.create! :firstname => "Jackson", :lastname => "Harms", :email => "jackson@blendinteractive.com", :display => true
-  Speaker.create! :firstname => "Tyler", :lastname => "Murtha", :email => "tyler.harms@gmail.com", :display => true
-  Speaker.create! :firstname => "Seth", :lastname => "Larson", :email => "seth.larson@gmail.com", :display => false
+  create_speakers_and_sessions
 end
 
 When(/^I go to the sessions display page$/) do
@@ -33,4 +30,12 @@ end
 
 Then(/^I should not see "(.*?)"$/) do |text|
   assert page.has_content?(text) == false
+end
+
+def create_speakers_and_sessions
+  jackson = Speaker.create! :firstname => "Jackson", :lastname => "Harms", :email => "jackson@blendinteractive.com", :display => true
+  tyler = Speaker.create! :firstname => "Tyler", :lastname => "Murtha", :email => "tyler.harms@gmail.com", :display => true
+  Speaker.create! :firstname => "Seth", :lastname => "Larson", :email => "seth.larson@gmail.com", :display => false
+  Session.create!(:speaker => jackson, :title => "Intro to Rails", :abstract => "If you like asp.net mvc but want to write less code check this out")
+  Session.create!(:speaker => tyler, :title => "Metaprogramming .Net", :abstract => "Understanding the concepts, advantages, and pitfalls of metaprogramming")
 end
--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class SessionTest < ActiveSupport::TestCase
   test "sessions should require a title" do
-    session = Session.new :speaker => "Jackson Harms", :abstract => "Testing with selenium"
+    speaker = Speaker.new firstname: "Jackson", lastname: "Harms", email: "jackson@blendisimo.com"
+    speaker.save!
+    session = Session.new :speaker => speaker, :abstract => "Testing with selenium"
     refute session.save, "Saved the session without a title"
     refute session.valid?
     assert session.errors.get(:title).any?
