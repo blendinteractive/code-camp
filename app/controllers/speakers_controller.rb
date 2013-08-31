84eb214e8d5eb59fbb2d77f33e8503e18b0c2725
Tyler Harms <tyler.harms@gmail.com>
2013-08-31 12:19:08 -0500

    adds tests for show route, adds show template

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,5 +1,10 @@
 class SpeakersController < ApplicationController
+  # /speakers
   def index
     @speakers = Speaker.all
   end
+  # # /speakers/:id
+  # def show
+  #   @speaker = Speaker.find(:id)
+  # end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
 describe SessionsController do
+  let(:speaker) {Speaker.create(firstname: "Seth", lastname: "Larson", email: "tyler@blendisimo.com")}
+  let(:session) {Session.create(speaker: speaker, title: "Speaker 2 test title")}
   describe "session routes" do
     it "routes get /sessions to sessions#index" do
       expect(:get => "/sessions").to route_to(
@@ -9,10 +11,10 @@ describe SessionsController do
       )
     end
     it "routes get /sessions/:id to sessions#show" do
-      expect(:get => "/sessions/1").to route_to(
+      expect(:get => "/sessions/" + session.id.to_s).to route_to(
         :controller => "sessions",
         :action => "show",
-        :id => "1"
+        :id => session.id.to_s
       )
     end
   end
@@ -34,6 +36,7 @@ describe SessionsController do
 
       session1 = Session.create!(speaker: session_speaker1, title: "Speaker 1 test title")
       session2 = Session.create!(speaker: session_speaker2, title: "Speaker 2 test title")
+
       get :index
 
       expect(assigns(:sessions)).to match_array([session1, session2])
@@ -42,9 +45,13 @@ describe SessionsController do
 
   describe "GET #show" do
     it "responds successfully with an HTTP 200 status code" do
-      get :show
+      get :show, :id => session.id
       expect(response).to be_success
       expect(response.status).to eq(200)
+    end
+    it "renders the show template" do
+      get :show, :id => session.id
+      expect(response).to render_template("show")
     end
   end
 end
